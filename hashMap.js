4a22a2dc10c30e91b280c307943feb985244e65c
John Young <john.young@spreadstreet.io>
2019-03-04 12:13:46 -0500

    Getting to palindromes

--- hashMap.js
+++ hashMap.js
@@ -2,88 +2,89 @@
 
 class HashMap {
   constructor(initialCapacity=8) {
-      this.length = 0;
-      this._slots = [];
-      this._capacity = initialCapacity;
-      this._deleted = 0;
+    this.length = 0;
+    this._slots = [];
+    this._capacity = initialCapacity;
+    this._deleted = 0;
   }
 
   get(key) {
-      const index = this._findSlot(key);
-      if (this._slots[index] === undefined) {
-          throw new Error('Key error');
-      }
-      return this._slots[index].value;
+    const index = this._findSlot(key);
+    if (this._slots[index] === undefined) {
+      throw new Error('Key error');
+    }
+    return this._slots[index].value;
   }
 
   set(key, value) {
-      const loadRatio = (this.length + this._deleted + 1) / this._capacity;
-      if (loadRatio > HashMap.MAX_LOAD_RATIO) {
-          this._resize(this._capacity * HashMap.SIZE_RATIO);
-      }
+    const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * HashMap.SIZE_RATIO);
+    }
 
-      const index = this._findSlot(key);
-      this._slots[index] = {
-          key,
-          value,
-          deleted: false
-      };
-      this.length++;
+    const index = this._findSlot(key);
+    this._slots[index] = {
+      key,
+      value,
+      deleted: false
+    };
+    this.length++;
   }
 
   remove(key) {
-      const index = this._findSlot(key);
-      const slot = this._slots[index];
-      if (slot === undefined) {
-          throw new Error('Key error');
-      }
-      slot.deleted = true;
-      this.length--;
-      this._deleted++;
+    const index = this._findSlot(key);
+    const slot = this._slots[index];
+    if (slot === undefined) {
+      throw new Error('Key error');
+    }
+    slot.deleted = true;
+    this.length--;
+    this._deleted++;
   }
 
   _findSlot(key) {
-      const hash = HashMap._hashString(key);
-      const start = hash % this._capacity;
+    const hash = HashMap._hashString(key);
+    const start = hash % this._capacity;
 
-      for (let i=start; i<start + this._capacity; i++) {
-          const index = i % this._capacity;
-          const slot = this._slots[index];
-          if (slot === undefined || (slot.key == key && !slot.deleted)) {
-              return index;
-          }
+    for (let i=start; i<start + this._capacity; i++) {
+      const index = i % this._capacity;
+      const slot = this._slots[index];
+      if (slot === undefined || (slot.key == key && !slot.deleted)) {
+        return index;
       }
+    }
   }
 
   _resize(size) {
-      const oldSlots = this._slots;
-      this._capacity = size;
-      // Reset the length - it will get rebuilt as you add the items back
-      this.length = 0;
-      this._deleted = 0;
-      this._slots = [];
+    const oldSlots = this._slots;
+    this._capacity = size;
+    // Reset the length - it will get rebuilt as you add the items back
+    this.length = 0;
+    this._deleted = 0;
+    this._slots = [];
 
-      for (const slot of oldSlots) {
-          if (slot !== undefined && !slot.deleted) {
-              this.set(slot.key, slot.value);
-          }
+    for (const slot of oldSlots) {
+      if (slot !== undefined && !slot.deleted) {
+        this.set(slot.key, slot.value);
       }
+    }
   }
 
   static _hashString(string) {
-      let hash = 5381;
-      for (let i=0; i<string.length; i++) {
-          hash = (hash << 5) + hash + string.charCodeAt(i);
-          hash = hash & hash;
-      }
-      return hash >>> 0;
+    let hash = 5381;
+    for (let i=0; i<string.length; i++) {
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      hash = hash & hash;
+    }
+    return hash >>> 0;
   }
 }
 
 HashMap.MAX_LOAD_RATIO = 0.9;
 HashMap.SIZE_RATIO = 3;
 
-let lotrHash = new HashMap();
+// Create a hash map call lor and add the following
+let lor = new HashMap();
 const chars = [
   {Hobbit: 'Bilbo'},
   {Hobbit: 'Frodo'},
@@ -96,6 +97,14 @@ const chars = [
   {LadyOfLight: 'Galadriel'},
   {HalfElven: 'Arwen'},
   {Ent: 'Treebeard'},
-]
-lotrHash.set('Hobbit', 'Bilbo');
-console.log(lotrHash);
+];
+chars.map(char => {
+  const entry = Object.entries(char);
+  return lor.set(entry[0][0], entry[0][1]);
+});
+
+// Retrieve the value that is hashed in the key Maiar
+console.log(lor.get('Maiar'));
+console.log(lor);
+
+module.exports = HashMap;
---
+++ palindrome.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const HashMap = require('./hashMap');
+const palindromeHash = new HashMap();
+
+// Is it possible to re-arrange these letters to make a palindrome?
+console.log(palindromeHash._hashString('acecarr'));
+
+// console.log(palindromeHash);
